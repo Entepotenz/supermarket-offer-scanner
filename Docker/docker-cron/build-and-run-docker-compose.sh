83e557e98b0c3f04520f03236ed81061832e9e3b
F.K <19738301+Entepotenz@users.noreply.github.com>
2023-05-11 22:27:18 +0200

    load s6 overlay version from file

---
+++ Docker/docker-cron/build-and-run-docker-compose.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd "$PWD"
+
+cd "$SCRIPT_DIR"
+
+S6_OVERLAY_VERSION=$(tr -d '\n' < "$SCRIPT_DIR/../../s6_overlay_version.txt")
+
+docker-compose build --build-arg S6_OVERLAY_VERSION="${S6_OVERLAY_VERSION}"
+
+popd

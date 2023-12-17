bcd9cda0077b8e6154d762ad5d66452841c6230f
F.K. <19738301+Entepotenz@users.noreply.github.com>
2023-12-17 16:57:09 +0100

    bla

--- source/Pushover.py
+++ source/Pushover.py
@@ -10,6 +10,7 @@ class Pushover:
 
     def send_notification(self, message: str) -> None:
         conn = http.client.HTTPSConnection("api.pushover.net:443")
+        print("test")
         conn.request(
             "POST",
             "/1/messages.json",

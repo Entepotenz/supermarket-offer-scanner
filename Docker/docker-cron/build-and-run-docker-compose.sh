0730f3a6141fa2c5a6fb64f892d34a771b9c12e4
F.K. <19738301+Entepotenz@users.noreply.github.com>
2024-01-02 11:15:00 +0100

    shfmt

--- Docker/docker-cron/build-and-run-docker-compose.sh
+++ Docker/docker-cron/build-and-run-docker-compose.sh
@@ -5,13 +5,13 @@ set -o pipefail
 set -o nounset
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 pushd "$PWD"
 
 cd "$SCRIPT_DIR"
 
-S6_OVERLAY_VERSION=$(tr -d '\n' < "$SCRIPT_DIR/../../s6_overlay_version.txt")
+S6_OVERLAY_VERSION=$(tr -d '\n' <"$SCRIPT_DIR/../../s6_overlay_version.txt")
 
 docker-compose build --build-arg S6_OVERLAY_VERSION="${S6_OVERLAY_VERSION}"
 
--- Docker/docker-one-shot/build-and-run-and-remove-docker.sh
+++ Docker/docker-one-shot/build-and-run-and-remove-docker.sh
@@ -5,14 +5,13 @@ set -o pipefail
 set -o nounset
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 IMAGE_NAME="supermarket-offer-scanner-one-shot"
 
-if [[ "$(docker images -q "$IMAGE_NAME" 2> /dev/null)" != "" ]]; then
+if [[ "$(docker images -q "$IMAGE_NAME" 2>/dev/null)" != "" ]]; then
   docker image rm "$IMAGE_NAME"
 fi
-
 
 (cd "$SCRIPT_DIR/../../" && docker build -f "$SCRIPT_DIR/Dockerfile" -t "$IMAGE_NAME" .)
 

454db23dd97388ea1c65eded0089f8fa951275fe
F.K. <19738301+Entepotenz@users.noreply.github.com>
2024-07-07 13:52:20 +0200

    streamline shell script docker image definition

--- run-poetry-update.sh
+++ run-poetry-update.sh
@@ -5,12 +5,14 @@ set -o pipefail
 set -o nounset
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
+DOCKER_IMAGE="python:3.12.3-slim@sha256:2be8daddbb82756f7d1f2c7ece706aadcb284bf6ab6d769ea695cc3ed6016743"
+
 docker run --rm \
   --pull always \
   -it \
   -v "$(pwd)/:/source" \
   -v "/source/.venv" \
-  python:3.12.3-slim@sha256:2be8daddbb82756f7d1f2c7ece706aadcb284bf6ab6d769ea695cc3ed6016743 bash -c "\
+  $DOCKER_IMAGE bash -c "\
     pip install poetry; \
     cd /source; \
     poetry self add poetry-plugin-export; \
